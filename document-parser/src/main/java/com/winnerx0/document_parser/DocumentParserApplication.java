50f92b286a7421ff9a2734e344cc01f1ece7feb7
winnerx0 <eragbonikpeya@gmail.com>
2025-08-16 17:09:06 +0100

    initial commit

---
+++ document-parser/src/main/java/com/winnerx0/document_parser/DocumentParserApplication.java
@@ -0,0 +1,13 @@
+package com.winnerx0.document_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentParserApplication.class, args);
+	}
+
+}
---
+++ document-parser/src/test/java/com/winnerx0/document_parser/DocumentParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winnerx0.document_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
